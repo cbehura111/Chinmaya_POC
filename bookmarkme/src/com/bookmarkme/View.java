8c5b51f171e8e375a5cec7ef19e4a6a62ff53a13
Chinmay <cbehura111@gmail.com>
2021-06-16 19:18:50 +0530

    View Class & Controller added to project

--- bookmarkme/src/com/bookmarkme/View.java
+++ bookmarkme/src/com/bookmarkme/View.java
@@ -6,7 +6,7 @@ import com.bookmarkme.entities.User;
 
 public class View {
 	public static void bookmark(User user,Bookmark[][] bookmarks) {
-		System.out.println("\n"+user.getEmail()+" is Bookmarking :");
+		//System.out.println("\n"+user.getEmail()+" is Bookmarking :");
 		for (int i = 0; i <DataStore.USER_BOOKMARK_LIMIT; i++) {
 			int typeOffset = (int) (Math.random()*DataStore.BOOKMARK_TYPE_COUNT);
 			int bookmarkOffset = (int) (Math.random()*DataStore.BOOKMARK_COUNT_PERTYPE);
