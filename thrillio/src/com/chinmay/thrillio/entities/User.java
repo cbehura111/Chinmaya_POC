fe6ca29a3c92f8d4b7d4b931bb7f06add054e5a4
Chinmay <cbehura111@gmail.com>
2021-09-08 11:46:37 +0530

    Revised Thrillio

--- thrillio/src/com/chinmay/thrillio/entities/User.java
+++ thrillio/src/com/chinmay/thrillio/entities/User.java
@@ -6,47 +6,61 @@ public class User {
 	private String password;
 	private String firstName;
 	private String lastName;
-	private String Gender;
+	private String gender;
 	private String userType;
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getGender() {
-		return Gender;
+		return gender;
 	}
+
 	public void setGender(String gender) {
-		Gender = gender;
+		this.gender = gender;
 	}
+
 	public String getUserType() {
 		return userType;
 	}
+
 	public void setUserType(String userType) {
 		this.userType = userType;
 	}
