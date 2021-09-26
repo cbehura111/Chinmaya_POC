060f7158798cbedd0a21f442db1cb3a1493d2cdb
Chinmay <cbehura111@gmail.com>
2021-09-26 22:08:03 +0530

    GIT TUTS BY ME

--- thrillio/src/com/chinmay/thrillio/exceptions/ExceptionsDemo.java
+++ thrillio/src/com/chinmay/thrillio/exceptions/ExceptionsDemo.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 public class ExceptionsDemo {
 
 	public static void main(String[] args) {
+
 		System.out.println("\n Inside Main Method...");
 		Share();
 		System.out.println("\n End of Main Method...");
@@ -14,7 +15,9 @@ public class ExceptionsDemo {
 		System.out.println("\n Inside Share Method...");
 
 		try {
-			HttpConnect.send(0, "Hello", "www.google.com");
+			HttpConnect.send(0
+
+					, "Hello", "www.google.com");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
