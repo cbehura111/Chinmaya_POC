46b5a5441810ba0c2c880f0365c937398c2fc59b
chinmaya.behura <chinmaya.behura@i-exceed.com>
2024-09-09 15:18:13 +0530

    added PaymentProcessing

---
+++ PaymentProcessing/src/test/java/com/chinmaya/code/payment/PaymentProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinmaya.code.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
